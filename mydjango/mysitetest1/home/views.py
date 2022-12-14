4449c908eedef68cd102831f82ad07bbcb83ecb3
Samuel Kalumba <kalumbasml@gmail.com>
2022-12-14 14:38:50 +0000

    minor update1

--- mydjango/mysitetest1/home/views.py
+++ mydjango/mysitetest1/home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+#from django.shortcuts import render
 
 def index(request):
-    return render(request, 'templates/home/index.html', {})
+    return HttpResponse("Hello World")

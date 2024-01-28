f522a723c8fd46b3dd5dd2c55a1af40eec74a010
Samuels7 <66149372+samuels97@users.noreply.github.com>
2024-01-28 22:42:10 +0300

    Update test2.py

--- test/test2.py
+++ test/test2.py
@@ -10,3 +10,32 @@ def greet(name):
     print(f"Hello, {name}!")
 
 greet('Sam')
+
+from datetime import datetime
+
+
+def log_datetime(func):
+    '''Log the date and time of a function'''
+
+    def wrapper():
+        print(f'Function: {func.__name__}\nRun on: {datetime.today().strftime("%Y-%m-%d %H:%M:%S")}')
+        print(f'{"-"*30}')
+        func()
+    return wrapper
+
+
+@log_datetime
+def daily_backup():
+
+    print('Daily backup job has finished.')   
+
+     
+daily_backup()
+
+# Output
+'''
+Daily backup job has finished.
+Function: daily_backup
+Run on: 2021-06-06 06:54:14
+---------------------------
+'''
